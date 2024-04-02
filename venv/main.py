37ee3a6337b47c0f25c23a6b99c3c09d77295016
Daten10 <tendanil563@gmail.com>
2024-04-02 13:59:00 +0600

    v1

---
+++ venv/main.py
@@ -0,0 +1,38 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from dotenv import load_dotenv
+from os import getenv
+import logging
+
+
+load_dotenv()
+bot = Bot(token=getenv('BOT_TOKEN'))
+dp = Dispatcher()
+
+# handlers
+@dp.message(Command('start'))
+async def start_cmd(message: types.Message):
+    await message.answer(f'привет! {message.from_user.first_name} я хвастаюсь')
+
+
+@dp.message(Command('picture'))
+async def send_picture(message: types.Message):
+    file = types.FSInputFile('venv/pictures/images.jpg')
+    await message.answer_photo(photo=file)
+
+
+@dp.message()
+async def echo(message: types.Message):
+    logging.info(message)
+    await message.answer(message.text)
+
+
+async def main():
+    # запуск бота
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
